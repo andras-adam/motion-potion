2c80e28cad4f70391c261b83f24170f75fe00de9
András Ádám <adm.andras@gmail.com>
2023-11-11 23:51:00 +0200

    Add useDetectMovement hook

--- src/screens/spell/StepTwo.tsx
+++ src/screens/spell/StepTwo.tsx
@@ -1,79 +1,17 @@
 import { useNavigation } from "@react-navigation/native";
 import { UseNavigation } from "../../types/navigation";
-import { useCallback, useEffect, useRef, useState } from "react";
-import {
-  detect_figure,
-  InputDatum,
-  Movement,
-} from "../../motionrecog/motion_recognition";
-import { Subscription } from "expo-sensors/build/Pedometer";
-import { DeviceMotion, DeviceMotionMeasurement } from "expo-sensors";
+import { Movement } from "../../motionrecog/motion_recognition";
 import { ImageScreen } from '../../components/ImageScreen'
+import { useDetectMovement } from '../../hooks/useDetectMovement'
 
 export function StepTwo() {
   const { navigate } = useNavigation<UseNavigation<"SpellStepTwo">>();
 
-  const motionData = useRef<InputDatum[]>([]);
-  const i = useRef<number>(0);
-
-  const listenerRef = useRef<Subscription[]>([]);
-
-  const handler = useCallback((data: DeviceMotionMeasurement) => {
-    if (data.acceleration) {
-      const datum = {
-        ax: data.acceleration.x,
-        ay: data.acceleration.y,
-        az: data.acceleration.z,
-        timestamp: data.interval,
-      };
-
-      motionData.current.push(datum);
-      i.current += 1;
-      if (motionData.current.length > 500) {
-        motionData.current.shift();
-      }
-    }
-    if (i.current % 10 == 0) {
-      i.current = 0;
-      let res = detect_figure(motionData.current);
-      for (const movement of res) {
-        if (movement == Movement.circle) {
-          if (listenerRef.current.length > 0) {
-            listenerRef.current[0].remove();
-          }
-          navigate("SpellStepThree");
-        }
-      }
-    }
-  }, []);
-
-  let [started, setStarted] = useState(false);
-
-  useEffect(() => {
-    let a = async () => {
-      await new Promise((resolve) => setTimeout(resolve, 300));
-      setStarted(true);
-    };
-    a();
-  });
-
-  useEffect(() => {
-    if (started) {
-      DeviceMotion.setUpdateInterval(1);
-      const listener = DeviceMotion.addListener(handler);
-      listenerRef.current.push(listener);
-      return () => listener.remove();
-    }
-  }, [started, handler]);
-
-  useEffect(() => {
-    (async () => {
-      const response = await DeviceMotion.requestPermissionsAsync();
-      if (!response.granted) {
-        console.warn("Device motion permission not granted");
-      }
-    })();
-  }, []);
+  // Detect movement
+  useDetectMovement({
+    movement: Movement.circle,
+    onComplete: () => navigate("SpellStepThree")
+  })
 
   return (
     <ImageScreen
