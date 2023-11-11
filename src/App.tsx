80c99d864fcba8f64d372780e4c46bae5e7cf128
András Ádám <adm.andras@gmail.com>
2023-11-11 22:50:43 +0200

    Dark default background

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000000",
     alignItems: "center",
     justifyContent: "center",
   },
