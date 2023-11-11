e27db8384b97a11d7edfc298050694665426845e
András Ádám <adm.andras@gmail.com>
2023-11-11 20:27:09 +0200

    Code clean up

--- src/screens/spell/StepFour.tsx
+++ src/screens/spell/StepFour.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, TouchableWithoutFeedback, View } from 'react-native'
+import { Image, StyleSheet, View } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { UseNavigation } from '../../types/navigation'
 import { useCallback, useEffect, useRef, useState } from 'react'
@@ -77,17 +77,6 @@ export function StepFour() {
   return (
     <View style={styles.screen}>
 
-      {/* Touchable hitbox for the image button */}
-
-      {/*<View style={styles.absolute}>*/}
-      {/*  <TouchableWithoutFeedback*/}
-      {/*    onPress={() => navigate('SpellStepFive')}*/}
-      {/*    style={styles.touchable}*/}
-      {/*  >*/}
-      {/*    <View style={styles.touchableContent}></View>*/}
-      {/*  </TouchableWithoutFeedback>*/}
-      {/*</View>*/}
-
       {/* Fullscreen image */}
 
       <Image
@@ -104,22 +93,6 @@ const styles = StyleSheet.create({
     flex: 1,
     position: 'relative',
     backgroundColor: '#000000'
-  },
-  absolute: {
-    position: 'absolute',
-    // borderColor: '#ff0000',
-    // borderWidth: 2,
-    bottom: 32,
-    left: 16,
-    right: 16,
-    height: 96,
-    zIndex: 100
-  },
-  touchable: {
-    flex: 1
-  },
-  touchableContent: {
-    flex: 1
   },
   image: {
     objectFit: 'cover',
