6b042b3088529c84a8c157fc457e8ec8f7b23272
Matti Ziemla <mtizim@protonmail.com>
2023-11-11 20:42:51 +0200

    shuffle routing

--- src/screens/spell/StepFive.tsx
+++ src/screens/spell/StepFive.tsx
@@ -1,19 +1,22 @@
-import { Image, StyleSheet, TouchableWithoutFeedback, View } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
-import { UseNavigation } from '../../types/navigation'
-
+import {
+  Image,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { UseNavigation } from "../../types/navigation";
 
 export function StepFive() {
-  const { navigate } = useNavigation<UseNavigation<'SpellStepFive'>>()
+  const { navigate } = useNavigation<UseNavigation<"SpellStepFive">>();
 
   return (
     <View style={styles.screen}>
-
       {/* Touchable hitbox for the image button */}
 
       <View style={styles.absolute}>
         <TouchableWithoutFeedback
-          onPress={() => navigate('Home')}
+          onPress={() => navigate("Map")}
           style={styles.touchable}
         >
           <View style={styles.touchableContent}></View>
@@ -23,39 +26,38 @@ export function StepFive() {
       {/* Fullscreen image */}
 
       <Image
-        source={require('../../../assets/spell/spell_step_5.png')}
+        source={require("../../../assets/spell/spell_step_5.png")}
         style={styles.image}
       />
-
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    position: 'relative',
-    backgroundColor: '#000000'
+    position: "relative",
+    backgroundColor: "#000000",
   },
   absolute: {
-    position: 'absolute',
+    position: "absolute",
     // borderColor: '#ff0000',
     // borderWidth: 2,
     bottom: 32,
     left: 16,
     right: 16,
     height: 96,
-    zIndex: 100
+    zIndex: 100,
   },
   touchable: {
-    flex: 1
+    flex: 1,
   },
   touchableContent: {
-    flex: 1
+    flex: 1,
   },
   image: {
-    objectFit: 'cover',
-    width: '100%',
-    height: '100%'
-  }
-})
+    objectFit: "cover",
+    width: "100%",
+    height: "100%",
+  },
+});
--- src/screens/spell/StepFour.tsx
+++ src/screens/spell/StepFour.tsx
@@ -1,14 +1,17 @@
-import { Image, StyleSheet, View } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
-import { UseNavigation } from '../../types/navigation'
-import { useCallback, useEffect, useRef, useState } from 'react'
-import { detect_figure, InputDatum, Movement } from '../../motionrecog/motion_recognition'
-import { Subscription } from 'expo-sensors/build/Pedometer'
-import { DeviceMotion, DeviceMotionMeasurement } from 'expo-sensors'
-
+import { Image, StyleSheet, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { UseNavigation } from "../../types/navigation";
+import { useCallback, useEffect, useRef, useState } from "react";
+import {
+  detect_figure,
+  InputDatum,
+  Movement,
+} from "../../motionrecog/motion_recognition";
+import { Subscription } from "expo-sensors/build/Pedometer";
+import { DeviceMotion, DeviceMotionMeasurement } from "expo-sensors";
 
 export function StepFour() {
-  const { navigate } = useNavigation<UseNavigation<'SpellStepFour'>>()
+  const { navigate } = useNavigation<UseNavigation<"SpellStepFour">>();
 
   const motionData = useRef<InputDatum[]>([]);
   const i = useRef<number>(0);
@@ -33,7 +36,6 @@ export function StepFour() {
     if (i.current % 20 == 0) {
       i.current = 0;
       let res = detect_figure(motionData.current);
-      console.log();
       for (const movement of res) {
         if (movement == Movement.line_y) {
           if (listenerRef.current.length > 0) {
@@ -75,27 +77,25 @@ export function StepFour() {
 
   return (
     <View style={styles.screen}>
-
       {/* Fullscreen image */}
 
       <Image
-        source={require('../../../assets/spell/spell_step_4.png')}
+        source={require("../../../assets/spell/spell_step_4.png")}
         style={styles.image}
       />
-
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    position: 'relative',
-    backgroundColor: '#000000'
+    position: "relative",
+    backgroundColor: "#000000",
   },
   image: {
-    objectFit: 'cover',
-    width: '100%',
-    height: '100%'
-  }
-})
+    objectFit: "cover",
+    width: "100%",
+    height: "100%",
+  },
+});
